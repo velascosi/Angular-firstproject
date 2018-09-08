618d9e8bb9a25d521c17ff4cdee5476c9c0f7d07
Silene Tarquino Velasco <velascosilene@gmail.com>
2018-09-07 22:19:36 -0300

    commit update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing';
 import { VideoListComponent } from './video-list/video-list.component';
 import { VideoDetailComponent } from './video-detail/video-detail.component';
+
+
+const appRoutes: Routes = 
+[
+  {path: "videos/:slug"}
+  {
+  path: "videos",
+  component: VideoListComponent,
+  },
+  {
+  path: "videos/:slug",
+  component: VideoDetailComponent,
+  }
+]
 
 @NgModule({
   declarations: [
@@ -17,7 +31,8 @@ import { VideoDetailComponent } from './video-detail/video-detail.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
