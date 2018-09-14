fedd4e4bb72d9ac3a9f2fa845214b497663ae72f
Silene Tarquino Velasco <velascosilene@gmail.com>
2018-09-14 00:17:11 -0300

    update debug localhost

--- src/app/video-list/video-list.component.ts
+++ src/app/video-list/video-list.component.ts
@@ -6,30 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video-list.component.css']
 })
 export class VideoListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    throw new Error("Method not implemented.");
+  }
+
+  ngOnDestroy() : void {}
+  
     tittle = "Video List";
-    someItem = "<h1>Hi there!</h1>"
+    someItem = "<h1>Hi there!</h1>";
     // videoList = ["Item 1", "Item 2", "Item 3"]
     videoList = [
       {
         name: "Item 1",
         slug: "Item-1",
-        embed: `1TfUyZxfs0c`,
+        embed: `1TfUyZxfs0c`
       },
       {
         name: "Item 2",
         slug: "Item-2",
-        embed: `1TfUyZxfs0c`,
+        embed: `1TfUyZxfs0c`
       },
       {
         name: "Item 3",
         slug: "Item-3",
-        embed: null,
+        embed: null
       }
-    }
- 
-  constructor() { }
-
-  ngOnInit() {
+    ]
   }
+ 
 
-}
